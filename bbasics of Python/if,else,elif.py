242d0dcf28f510091d05e577f138ddfcfe77a8e4
Nityasree <159861640+SNityasree@users.noreply.github.com>
2024-09-01 21:35:05 +0530

    Create if,else,elif.py

---
+++ bbasics of Python/if,else,elif.py
@@ -0,0 +1,11 @@
+mark=int(input("Enter mark:"))
+if(mark<35):
+    print("Poor Student")
+elif(mark>35 and mark<70):
+    print("Average Student")
+else:
+    print("Good Student")
+if(mark>70):
+    a=input("Enter your name:")
+    
+print("Congradulation",a,"you got a good score")
