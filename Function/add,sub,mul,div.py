e432b5f9d4c2728d518e91152537c7887502d88a
Nityasree <159861640+SNityasree@users.noreply.github.com>
2024-09-04 00:05:43 +0530

    Create add,sub,mul,div.py

---
+++ Function/add,sub,mul,div.py
@@ -0,0 +1,14 @@
+def add(n1,n2):
+    print(n1+n2)
+def sub(n1,n2):
+    print(n1-n2)
+def mul(n1,n2):
+    print(n1*n2)
+def div(n,n2):
+    print(n//n2)
+n1=int(input())
+n2=int(input())
+(add(n1,n2))
+(sub(n1,n2))
+(mul(n1,n2))
+(div(n1,n2))
